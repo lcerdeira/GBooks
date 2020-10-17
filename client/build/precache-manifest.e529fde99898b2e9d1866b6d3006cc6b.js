2308fcfd5bd99d64f42b2b7a9c741934e30823f6
Louise Cerdeira <lcerdeira@gmail.com>
2020-10-17 21:48:00 +1100

    heroku

---
+++ client/build/precache-manifest.e529fde99898b2e9d1866b6d3006cc6b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "014a702367fb3c3af012",
+    "url": "/static/css/main.913cf0fa.chunk.css"
+  },
+  {
+    "revision": "014a702367fb3c3af012",
+    "url": "/static/js/main.27334e86.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "44ae3db1358c6f06d7c0",
+    "url": "/static/js/2.7a2987f7.chunk.js"
+  },
+  {
+    "revision": "70bde038f9e064e1ea96d2c52d74a9b8",
+    "url": "/index.html"
+  }
+];
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,56 +9,56 @@ import { Col, Row, Container } from "../components/Grid";
 import { List } from "../components/List";
 
 class Home extends Component {
-  state = {
-    books: [],
-    q: "",
-    message: "Search For A Book To Begin!"
-  };
+    state = {
+        books: [],
+        q: "",
+        message: "Lets begin, please search for a book and enjoy!"
+    };
 
-  handleInputChange = event => {
-    const { name, value } = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  getBooks = () => {
-    API.getBooks(this.state.q)
-      .then(res =>
+    handleInputChange = event => {
+        const { name, value } = event.target;
         this.setState({
-          books: res.data
-        })
-      )
-      .catch(() =>
-        this.setState({
-          books: [],
-          message: "No New Books Found, Try a Different Query"
-        })
-      );
-  };
+            [name]: value
+        });
+    };
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    this.getBooks();
-  };
+    getBooks = () => {
+        API.getBooks(this.state.q)
+            .then(res =>
+                this.setState({
+                    books: res.data
+                })
+            )
+            .catch(() =>
+                this.setState({
+                    books: [],
+                    message: "No New Books Found, please try a different book name to search"
+                })
+            );
+    };
 
-  handleBookSave = id => {
-    const book = this.state.books.find(book => book.id === id);
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.getBooks();
+    };
 
-    API.saveBook({
-      googleId: book.id,
-      title: book.volumeInfo.title,
-      subtitle: book.volumeInfo.subtitle,
-      link: book.volumeInfo.infoLink,
-      authors: book.volumeInfo.authors,
-      description: book.volumeInfo.description,
-      image: book.volumeInfo.imageLinks.thumbnail
-    }).then(() => this.getBooks());
-  };
+    handleBookSave = id => {
+        const book = this.state.books.find(book => book.id === id);
 
-  render() {
-    return (
-      <Container>
+        API.saveBook({
+            googleId: book.id,
+            title: book.volumeInfo.title,
+            subtitle: book.volumeInfo.subtitle,
+            link: book.volumeInfo.infoLink,
+            authors: book.volumeInfo.authors,
+            description: book.volumeInfo.description,
+            image: book.volumeInfo.imageLinks.thumbnail
+        }).then(() => this.getBooks());
+    };
+
+    render() {
+        return ( 
+<Container>
         <Row>
           <Col size="md-12">
             {/* <Jumbotron> */}
@@ -114,5 +114,4 @@ class Home extends Component {
     );
   }
 }
-
 export default Home;
